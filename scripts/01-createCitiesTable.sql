670b25ba50f403dcaef8fe4eb3668953bb9c2302
silvia <villalobos.veas@gmail.com>
2022-10-05 11:03:59 -0300

    seguimos con base de datos

--- scripts/01-createCitiesTable.sql
+++ scripts/01-createCitiesTable.sql
@@ -3,7 +3,7 @@
 DROP TABLE IF EXISTS cities;
 
 CREATE TABLE cities(
-    id INTEGER,
+    id INTEGER,    
     name VARCHAR(30),
     country VARCHAR(30),
     population INTEGER,
---
+++ scripts/02-steedCitiesTable.sql
@@ -0,0 +1,4 @@
+\c bigcities
+\COPY cities FROM 'C:/Users/villa/intro_dev/programming-intro-lab-13-silvi2021/data/ciudades.csv' csv HEADER;
+
+SELECT * FROM cities;
